86d174f0a26330325a4662aadcf1b544ee88e01b
AL KAFI SOHAG <85807115+al-kafi-sohag@users.noreply.github.com>
2025-07-17 21:56:34 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomePageController;
 
 Route::get('/', [HomePageController::class, 'index'])->name('home');
-
+Route::get('/new', [HomePageController::class, 'new'])->name('new');
