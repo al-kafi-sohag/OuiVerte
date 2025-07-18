07e58822dee759dd03b2e9d138faac7cca39014c
AL KAFI SOHAG <85807115+al-kafi-sohag@users.noreply.github.com>
2025-07-18 14:15:06 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomePageController;
 
 Route::get('/', [HomePageController::class, 'index'])->name('home');
-Route::get('/the-future-is-fractional', [HomePageController::class, 'new'])->name('new');
+Route::get('/the-future-is-fractional', [HomePageController::class, 'test1'])->name('test1');
+Route::get('/the-european-sovereign-cloud-opportunity', [HomePageController::class, 'test2'])->name('test2');
