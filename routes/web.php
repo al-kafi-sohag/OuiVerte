9c142930b1650d47fa4c5322a1d2df28a2804f94
AL KAFI SOHAG <85807115+al-kafi-sohag@users.noreply.github.com>
2025-07-26 02:07:42 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/infographics/future-proofing-the-european-grid', [HomePageControlle
 
 
 //Only for staging 
-if(config('app.env' != 'production'){
+if(config('app.env') != 'production'){
    Route::get('/infographics/germany-grid-infographic', [HomePageController::class, 'info4'])->name('infographics.4');
    Route::get('/infographics/analysis-of-ouiVertes-business-plan', [HomePageController::class, 'info5'])->name('infographics.5');
 }
