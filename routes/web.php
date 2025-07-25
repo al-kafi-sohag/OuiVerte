b2b8f700dcbcddd374566bb82b339b069d703d92
AL KAFI SOHAG <85807115+al-kafi-sohag@users.noreply.github.com>
2025-07-26 02:04:42 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,15 @@ Route::get('/', [HomePageController::class, 'index'])->name('home');
 Route::get('/infographics/the-future-is-fractional', [HomePageController::class, 'info1'])->name('infographics.1');
 Route::get('/infographics/the-european-sovereign-cloud-opportunity', [HomePageController::class, 'info2'])->name('infographics.2');
 Route::get('/infographics/future-proofing-the-european-grid', [HomePageController::class, 'info3'])->name('infographics.3');
-Route::get('/infographics/germany-grid-infographic', [HomePageController::class, 'info4'])->name('infographics.4');
+
+
+
+
+
+
+
+//Only for staging 
+if(config('app.env' != 'production'){
+   Route::get('/infographics/germany-grid-infographic', [HomePageController::class, 'info4'])->name('infographics.4');
+   Route::get('/infographics/analysis-of-ouiVertes-business-plan', [HomePageController::class, 'info5'])->name('infographics.5');
+}
