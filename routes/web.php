31a87b81b3aa8b7aff8df5fc88c3386b95851d8a
AL KAFI SOHAG <85807115+al-kafi-sohag@users.noreply.github.com>
2025-07-25 17:09:47 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomePageController;
 
 Route::get('/', [HomePageController::class, 'index'])->name('home');
-Route::get('/infographics/the-future-is-fractional', [HomePageController::class, 'test1'])->name('infographics.1');
-Route::get('/infographics/the-european-sovereign-cloud-opportunity', [HomePageController::class, 'test2'])->name('infographics.2');
-Route::get('/infographics/future-proofing-the-european-grid', [HomePageController::class, 'test3'])->name('infographics.3');
+Route::get('/infographics/the-future-is-fractional', [HomePageController::class, 'info1'])->name('infographics.1');
+Route::get('/infographics/the-european-sovereign-cloud-opportunity', [HomePageController::class, 'info2'])->name('infographics.2');
+Route::get('/infographics/future-proofing-the-european-grid', [HomePageController::class, 'info3'])->name('infographics.3');
+Route::get('/infographics/germany-grid-infographic', [HomePageController::class, 'info4'])->name('infographics.4');
