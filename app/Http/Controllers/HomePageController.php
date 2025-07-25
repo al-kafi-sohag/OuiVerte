7a45a5a4dbe03e7bf50c30928197d3d37c838245
AL KAFI SOHAG <85807115+al-kafi-sohag@users.noreply.github.com>
2025-07-25 17:10:33 +0600

    Update HomePageController.php

--- app/Http/Controllers/HomePageController.php
+++ app/Http/Controllers/HomePageController.php
@@ -12,19 +12,24 @@ class HomePageController extends Controller
         return view('frontend.home.new');
     }
 
-    public function test1()
+    public function info1()
     {
         return view('frontend.home.test1');
     }
 
-    public function test2()
+    public function info2()
     {
         return view('frontend.home.test2');
     }
 
-    public function test3()
+    public function info3()
     {
         return view('frontend.home.test3');
+    }
+
+    public function info4()
+    {
+        return view('frontend.home.info4');
     }
 
 
